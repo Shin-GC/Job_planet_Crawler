f4c2f4013cf143590c332d4c5335198ae2c11f77
Shin-GC <tlsrhkdcjs12@naver.com>
2021-01-26 22:27:47 +0900

    Function Max_page

--- JobPlanetCrawler.py
+++ JobPlanetCrawler.py
@@ -7,10 +7,10 @@ driver = webdriver.Chrome('/Users/SGC/Desktop/Job_Planet_Crawler/chromedriver')
 PAGE=1
 
 
-def get_soup(search='python',start_year=0,end_year=0,start_star=0,end_star=5):
+def get_soup(search='python',start_year=0,end_year=0,start_star=0,end_star=5,page=1):
     start_star=start_star*10
     end_star=end_star*10
-    URL=f"https://www.jobplanet.co.kr/job/search?page={PAGE}&yoe={start_year}%3B{end_year}&q={search}&rs={start_star}%3B{end_star}"
+    URL=f"https://www.jobplanet.co.kr/job/search?page={PAGE*page}&yoe={start_year}%3B{end_year}&q={search}&rs={start_star}%3B{end_star}"
     print(URL)
     driver.get(URL)
     html=driver.page_source
@@ -44,3 +44,13 @@ def get_Job_Planet(soup):
     score=get_score(soup)
     return {'title':title,'company':company,'score':score}
 
+def get_max_page(soup):    
+    max_page=soup.select_one('#job_search_app > div > div.job_search_content > div.job_search_list > div.jply_pagination_ty1 > button:nth-child(5)').get_text()
+    max_page=int(max_page)+1
+    return max_page
+
+maxpage=get_max_page(get_soup())
+
+for i in range (1,maxpage):
+    print(get_title(get_soup(page=i)))
+    time.sleep(3)
