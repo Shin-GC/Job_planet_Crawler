7aebebda1728f7a1c0fccb15494496ff900cbf53
Shin-GC <tlsrhkdcjs12@naver.com>
2021-01-28 23:39:29 +0900

    Add Excel max_page

--- JobPlanetExcel.py
+++ JobPlanetExcel.py
@@ -4,14 +4,8 @@ from openpyxl.styles import Font, Fill,Alignment,PatternFill
 wb=Workbook()
 ws=wb.active
 
-save=jp.get_Job_Planet(jp.get_soup(start_star=3))
-title=save['title']
-company=save['company']
-score=save['score']
-#title=jp.get_Job_Planet(jp.get_soup(start_star=3))['title']
-#company=jp.get_Job_Planet(jp.get_soup(start_star=3))['company']
-#score=jp.get_Job_Planet(jp.get_soup(start_star=3))['score']
 
+#검색한 공고의 최대 페이지
 blue_fill=PatternFill(start_color='1E90FF',end_color='1E90FF',fill_type='solid')
 #파란색으로 채우기
 def cell_style():
@@ -27,15 +21,35 @@ def cell_style():
 #맨 위 구분 셀을 파란색으로 체워주기
 cell_style()
 
+titles=[]
+companys=[]
+scores=[]
+title=[]
+company=[]
+score=[]
+
+maxpage=jp.get_max_page(jp.get_soup(search='python'))
+for i in range(1,maxpage):
+    save=jp.get_Job_Planet(jp.get_soup(search='python',page=i))
+    titles.append(save['title'])
+    companys.append(save['company'])
+    scores.append(save['score'])
+    #title=jp.get_Job_Planet(jp.get_soup(start_star=3))['title']
+    #company=jp.get_Job_Planet(jp.get_soup(start_star=3))['company']
+    #score=jp.get_Job_Planet(jp.get_soup(start_star=3))['score']
+title=sum(titles,title)
+company=sum(companys,company)
+score=sum(scores,score)
+
 for t in range(len(title)):
     ws.append([title[t]])
-#엑셀 title A셀에 값 넣기
+    #엑셀 title A셀에 값 넣기
 for c in range(2,len(company)+2):
     ws[f"B{c}"]=company[c-2]
-#엑셀 company 값 B 셀에 넣기
+    #엑셀 company 값 B 셀에 넣기
 for s in range(2,len(score)+2):
     ws[f"C{s}"]=score[s-2]
-#엑셀 score 값 C 셀에 넣기
+    #엑셀 score 값 C 셀에 넣기
 
 
 for x in range(1,len(title)+2):
@@ -56,15 +70,15 @@ for i in range(len(company)):
 
 for i in range(len(score)):
     int_score.append(len(score[i]))
-
+# 너비를 정해주기 위해 글씨의 길이를 리스트에 저장
 max_a_width=max(int_title)
 max_b_width=max(int_company)
 max_c_width=max(int_score)
-
+# 저장한 글씨 길이 리스트 중 가장 긴 것을 지정
 ws.column_dimensions['A'].width = max_a_width+15
 ws.column_dimensions['B'].width = max_b_width*2
 ws.column_dimensions['C'].width = max_c_width+10
-# 최대 길이를 잰 후 엑셀 너비 지정
+# 최대 길이를 잰 후 엑셀 너비 지정 [ 폰트 크기를 위해 뒤에 숫자로 추가너비 기입 ]
 
 wb.save('JobPlanet.xlsx')
 wb.close()
